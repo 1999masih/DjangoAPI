30c4e64b7720dea548e8de155810692f2b166c91
masihsaeedi <masihsaeedi.s@gmail.com>
2023-09-02 15:07:13 +0330

    [BlogAPI] added Initial setting for DRF

--- Blog API/django_proeject/settings.py
+++ Blog API/django_proeject/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #3rd Party
+    'rest_framework',
+
 
     #Local
     'accounts.apps.AccountsConfig',
@@ -128,3 +131,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+    "rest_framework.permissions.AllowAny",
+],
+}
